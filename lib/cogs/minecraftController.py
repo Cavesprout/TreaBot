e5bf7e25a4e5815d9e7220fe2e6dc28099d3e2e2
Cavesprout <thecaveisstilldark@gmail.com>
2021-03-26 14:07:31 -0400

    Adding ping cog

---
+++ lib/cogs/minecraftController.py
@@ -0,0 +1,24 @@
+import discord
+
+from discord.ext.commands import Cog
+from discord.ext.commands import command
+from discord.ext import commands
+
+class MinecraftController(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @Cog.listener()
+    async def on_ready(self):
+        await self.bot.stdout.send("Minecraft Controller Online")
+        print("Minecraft Controller Online")
+        if not self.bot.ready:
+            self.bot.cogs_ready.ready_up("minecraftController")
+
+    @command()
+    async def ping(self, ctx):
+        await ctx.send(f'Pong! ({round(self.bot.latency * 1000)} ms)')
+
+
+def setup(bot):
+    bot.add_cog(MinecraftController(bot))
